3fe9a1812a2a2dd1116e4b6e93bf716d2ce52424
KrUtHi96 <kruthi.h.k@gmail.com>
2018-04-01 10:08:21 +0530

    Modularised code and Working GA

---
+++ Approximate APSP Methods/Overlap_Score_Pigeonhole.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+class OverlapScorePigeonhole:
+    
+    def __init__(self, reads, overlap_minimum = 12, max_error = 3):
+        
+        self.overlap_minimum = overlap_minimum
+        self.max_error = max_error
+        
+        self.n_pieces = max_error + 1
+        self.piece_size = self.overlap_minimum // self.n_pieces
+        
+        self.reads = reads
+        self.n_reads = len(reads)
+        
+        self.read_len = len(reads[0])
+
+
+    def divide(self, text):
+        pieces = [text[i: i + self.piece_size] for i in range(0, self.overlap_minimum, self.piece_size)]
+        
+        return pieces
+
+    
+    def build_index(self):
+        
+        index = [None] * self.n_pieces
+        read_n = 1
+        
+        for read in self.reads:
+            for i in range(self.n_pieces):
+                
+                start = i * self.piece_size
+                
+                piece = read[start:start + self.piece_size]
+                
+                if index[i] == None:
+                    index[i] = {}
+                if piece not in index[i]:
+                    index[i][piece] = []
+                    
+                index[i][piece].append(read_n)
+            read_n += 1
+        return index
+
+
+
+    def get_suffixes(self, read):
+        
+        N = self.read_len
+        suffixes = [read[i: ] for i in range(N - self.overlap_minimum + 1)]
+        
+        return suffixes
+
+
+    def overlap_scores(self):
+        
+        matrix = np.zeros(shape = [self.n_reads, self.n_reads])
+        
+        index = self.build_index()
+        
+        for read_index in range(self.n_reads):
+            
+            for S in self.get_suffixes(self.reads[read_index]):
+                
+                pieces = self.divide(S[:self.overlap_minimum])
+                
+                for i in range(self.n_pieces):
+                    
+                    if pieces[i] in index[i]:
+                        
+                        Li = index[i][pieces[i]]
+                        
+                        for read_no in Li:
+                            
+                            temp, end = 0, (i * self.piece_size)                        
+                            s1, s2 = S[:end], self.reads[read_no - 1][:end]
+                            
+                            for char_index in range(end):
+                                if(s1[char_index] != s2[char_index]):
+                                    temp += 1
+                                    
+                            if temp < self.max_error:
+                                
+                                temp1, reached_end, start = 0, True, i * self.piece_size + self.piece_size
+                                s1, s2 = S[start:], self.reads[read_no - 1][start:]
+                                
+                                for char_index in range(len(s1)):
+                                    if temp1 == self.max_error:
+                                        reached_end = False
+                                        break
+                                    if s1[char_index] != s2[char_index]:
+                                        temp1 += 1
+                                        
+                                if (reached_end) and (temp1 < self.max_error):
+                                    if read_index + 1 != read_no:
+                                        score = len(S)
+                                        matrix[read_index, read_no - 1] = score - temp
+                                        #print(read_index + 1," -> ", read_no, "Score :", score, "Error : ", temp)
+                                        
+                            
+                        
+                        break
+        return matrix
+
+if __name__ == '__main__':
+
+    reads = [
+             'AACCTTTCACGGTCACCCGCGG',
+             'TTTCACGGTCACCCAGTCAACC',
+             'GGTTAAACCCGGTAACCGTCAT',
+             'AACCTTGTGCTCCCAACGTAAA',
+             'GGTTCCAAACACTTGGTCAATC',
+             'TTGGAACCTTTCACGGTCACCC'
+             ]     
+    
+    osp = OverlapScorePigeonhole(reads)      
+    
+    matrix = osp.overlap_scores()
+    
+    print(matrix)
--- Dataset/generate_dataset.py
+++ Dataset/generate_dataset.py
@@ -1,70 +1,80 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 22 20:03:03 2018
-
-@author: KRUTHI
-"""
 from scipy.stats import truncnorm
 import matplotlib.pyplot as plt
 import random
 
-scale = 3
-extreme = 10
-size = 10000
-genes = ['A', 'G', 'T', 'C']
+class GenerateDataset:
+    
+    def __init__(self, error_rate, mutation_rate):
+        
+        self.scale = 3
+        self.extreme = 10
+        self.genes = ['A', 'G', 'T', 'C']
+        
+        self.error_rate = error_rate
+        self.mutation_rate = mutation_rate
+        
+        self.genome = ''
+        self.reads = []
 
-def int_to_gene(n):
-    if -10 <= n < -2.5:
-        return 'A'
-    elif -2.5 <= n < 0:
-        return 'G'
-    elif 0 <= n < 2.5:
-        return 'T'
-    else:
-        return 'C'
+    def int_to_gene(self, n):
+        if -10 <= n < -2.5:
+            return 'A'
+        elif -2.5 <= n < 0:
+            return 'G'
+        elif 0 <= n < 2.5:
+            return 'T'
+        else:
+            return 'C'
     
-def induce_error(read, error_rate, mutation_rate):
-    
-    for i in range(int(len(read) * mutation_rate)):
-        random_index = random.randint(0, len(read) - 1)
+    def induce_error(self, read):
         
-        rn = random.random()
-        
-        if rn < error_rate:
-            read = read[:random_index] + random.choice([c for c in genes if genes != read[random_index]]) + read[random_index+1:]
-        
-         
-    return read
+        for i in range(int(len(read) * self.mutation_rate)):
+            random_index = random.randint(0, len(read) - 1)
+            
+            rn = random.random()
+            
+            if rn < self.error_rate:
+                read = read[:random_index] + random.choice([c for c in self.genes if self.genes != read[random_index]]) + read[random_index+1:]            
+             
+        return read
 
-def random_genome(length = 1000):
+    def random_genome(self, length = 1000):
+        
+        a = -self.extreme / self.scale
+        b = self.extreme / self.scale
+        
+        numbers = truncnorm(a = a, b = b, scale = self.scale).rvs(size = length) 
+        numbers = numbers.round().astype(int)
+        
     
-    numbers = truncnorm(a = -extreme/scale, b = extreme/scale, scale = scale).rvs(size = size) 
-    numbers = numbers.round().astype(int)
-    
+        #plt.hist(numbers, 2 * self.extreme + 1)
+        
+        self.genome = ''.join([self.int_to_gene(n) for n in numbers])
+        
+        return self.genome
 
-    plt.hist(numbers, 2 * extreme + 1)
+    def random_reads(self, length = 20, num = 5):
     
-    genome = ''.join([int_to_gene(n) for n in numbers])
+        for i in range(num):
+            start = random.randint(0, len(self.genome) - length)
+            
+            read = self.genome[start:start + length]
+            
+            read = self.induce_error(read)
+            
+            self.reads.append(read)
+            
+        return self.reads
+
+if __name__ == '__main__':
     
-    return genome
+    gd = GenerateDataset(0.8, 0.6)
 
-def random_reads(genome, length = 20, num = 5):
-    reads = []
-    for i in range(num):
-        start = random.randint(0, len(genome) - length)
-        
-        read = genome[start:start + length]
-        
-        read = induce_error(read, 0.8, 0.6)
-        
-        reads.append(read)
-        
-    return reads
-
-genome = random_genome()
-print(genome)
-reads = random_reads(genome)
-count = 1
-for read in reads:
-    print("Read", count, read)
-    count += 1
+    genome = gd.random_genome(length = 5000)
+    
+    reads = gd.random_reads(length = 50, num = 10)
+    
+    count = 1
+    for read in reads:
+        print("Read", count, read)
+        count += 1

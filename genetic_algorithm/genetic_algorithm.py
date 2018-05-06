398e575daef88c1741756f0b65364b55326aae77
KrUtHi96 <kruthi.h.k@gmail.com>
2018-05-06 11:01:36 +0530

    Optimised code

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -1,25 +1,20 @@
 import random
-from matplotlib import pyplot as plt
-
-from generate_dataset import generate_dataset
-from overlap_methods import overlap_score_pigeonhole
-from evaluation_methods import shingles_score
 import genetic_algorithm_utilities
 
 
 def GeneticAlgorithm(size=100, generations=500, select_n=60):
 
-    reads = ["ac", "bc", "ab", "baabba"]
-    reads1 = ["x", "y", "ax", "yb"]
-
+    reads = ["abc", "d", "ef"]
+    reads1 = ["ab", "cde", "fg"]
 
     gau = genetic_algorithm_utilities.GeneticAlgorithmUtil(reads, reads1)
 
     population = gau.initialize_population(size)
-    print("population is initialised...")
-    print(population)
 
-    max_so_far = {'genome': '', 'genome1':'', 'score': -10000}
+    # print("population is initialised...")
+    # print(population)
+
+    max_so_far = {'genome': '', 'genome1': '', 'score': -10000}
 
     count = 1
     while count <= generations:
@@ -29,42 +24,30 @@ def GeneticAlgorithm(size=100, generations=500, select_n=60):
             score = gau.fitness_score(population[i])
 
             index_list = population[i]
-            print(index_list)
-            genome1 = ''.join([reads1[j] for j in index_list])
-
-            print("genome1", genome1, population[i])
-
+            genome1 = gau.generate_genome(index_list, 1)
 
             if score > max_so_far['score']:
                 max_so_far['genome'], max_so_far['genome1'], max_so_far['score'] = i, genome1, score
                 print("Max Score so far :", max_so_far['score'])
+
             if i == genome1:
-                print("Success : ", i, population[i], genome1)
+                print("Success : ", i, population[i])
                 exit()
 
         population, fitness = gau.selection(population, select_n, gau.fitness_score)
+        # print("Selection done")
 
-        print("Selection done")
-
+        # population regeneration
         while len(population) < size:
-            # print(len(population), size)
             temp = list(population.values())
             a = random.choice(temp)
             b = random.choice(temp)
             if a != b:
-                # print("CHeck", a, b)
+                genome_new, index_list = gau.crossover(a, b)
 
-                # start = random.randint(1, len(a) - 2)
-                # end = random.randint(start + 1, len(a))
-                # genome_new, index_list = gau.crossover1(a, b, start, end)
-                point_of_crossover = random.randint(0, len(a))
-                index_list = a[:point_of_crossover] + b[point_of_crossover:]
-                genome_new = gau.generate_genome(index_list, 0)
-                # genome_new, index_list = gau.crossover_edge_recombination(a, b)
                 if genome_new not in population:
                     population[genome_new] = index_list
-
-        print("Crossover done")
+        # print("Crossover done")
 
         # mutation
         for i in list(population.keys()):
@@ -74,11 +57,8 @@ def GeneticAlgorithm(size=100, generations=500, select_n=60):
                 population.pop(i)
                 population[temp_gen] = temp
 
-        print("Generation :", count,max_so_far['score'])
-
+        print("##### Generation : {} has max score {} ########".format(count, max_so_far['score']))
         count += 1
-
-        # print("Population len", len(population))
 
     return max_so_far['genome'], max_so_far['score']
 
--- genetic_algorithm/genetic_algorithm_utilities.py
+++ genetic_algorithm/genetic_algorithm_utilities.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class GeneticAlgorithmUtil:
@@ -8,104 +9,40 @@ class GeneticAlgorithmUtil:
         self.reads1 = reads1
         self.reads_n = len(reads)
 
-    # def add(self, i, j, choice):
-    #     if choice == 0:
-    #         return self.reads[i] + self.reads[j]
-    #     else:
-    #         return self.reads1[i] + self.reads1[j]
-
     def generate_genome(self, index_list, choice):
-        if choice == 0:
-            genome = ''.join([self.reads[j] for j in index_list])
-        else:
-            genome = ''.join([self.reads1[j] for j in index_list])
-
+        first = (choice == 0)
+        genome = ''.join([self.reads[j] if first else self.reads1[j] for j in index_list])
 
         return genome
 
-    # def initialize_population(self, size):
-    #     i = 0
-    #     population = {}
-    #     while i < size:
-    #         index_list = []
-    #         temp = list(range(self.reads_n))
-    #         while temp:
-    #             index = random.choice(temp)
-    #             temp.remove(index)
-    #             index_list.append(index)
-    #         genome = self.generate_genome(index_list)
-    #         if genome not in population:
-    #             population[genome] = index_list
-    #             i += 1
-    #     return population
-
-    # def initialize_pop(self, size, gen_len):
-    #     i = 0
-    #     population = dict()
-    #     while i < size:
-    #         genome = ''
-    #         index_list = []
-    #         temp = list(range(self.reads_n))
-    #         while len(genome) < gen_len:
-    #             index = random.choice(temp)
-    #             temp.remove(index)
-    #             index_list.append(index)
-    #             genome += self.reads[index]
-    #
-    #         if genome not in population:
-    #             population[genome] = index_list
-    #             i += 1
-    #     return population
-
-    def initialize_population(self, size):
-        import time
+    def initialize_population(self, size, length = 10):
         random.seed(time.time())
         i = 0
         population = dict()
         while i < size:
             genome = ''
             index_list = []
-            genome_len = random.randint(1, 10)
+            genome_len = random.randint(1, length)
             while len(genome) < genome_len:
-                index = random.choice(list(range(len(self.reads))))
+                index = random.choice(list(range(self.reads_n)))
                 index_list.append(index)
                 genome += self.reads[index]
 
             if genome not in population:
-                print(genome)
+                # print(genome)
                 population[genome] = index_list
                 i += 1
-        return population
 
-    # def fitness_score1(self, index_list):
-    #     score = 0
-    #     for i in range(len(index_list) - 1):
-    #         score += self.overlap_matrix[index_list[i], index_list[i + 1]]
-    #     return score
-    #
-    # def fitness_score2(self, index_list):
-    #     score = 0
-    #     for i in range(len(index_list) - 1):
-    #         for j in range(len(index_list) - 1):
-    #             score = score + (abs(i - j) * self.overlap_matrix[index_list[i], index_list[j]])
-    #     return score
+        return population
 
     def fitness_score(self, index_list):
 
-        genome = self.generate_genome(index_list, 0)
-        genome1 = self.generate_genome(index_list, 1)
+        genome, genome1 = self.generate_genome(index_list, 0), self.generate_genome(index_list, 1)
+        length, length1 = len(genome), len(genome1)
 
-        # print(genome, genome1)
+        score = 0 - abs(length - length1)
+        score += sum([1 if genome[i] == genome1[i] else 0 for i in range(min(length, length1))])
 
-        length = len(genome)
-        length1 = len(genome1)
-
-        # print(length, length1)
-
-        score = 0 - abs(len(genome) - len(genome1))
-        for i in range(min(length, length1)):
-            if genome[i] == genome1[i]:
-                score += 1
         return score
 
     def selection(self, population, n, fn):
@@ -120,113 +57,36 @@ class GeneticAlgorithmUtil:
             new_population[genome] = population[genome]
         return new_population, fitness
 
-    def crossover1(self, p1, p2, start, end):
-        temp = p1[start: end + 1]
-        count = 0
-        for i in p2:
-            if i not in temp:
-                if count < start:
-                    temp = [i] + temp
-                else:
-                    temp = temp + [i]
-                count += 1
-        # temp = self.mutation_pop(temp)
-        genome = self.generate_genome(temp, 0)
-        return genome, temp
-
     def mutation(self, index_list):
         t = len(index_list)
-        import time
         random.seed(time.time())
+
         a = random.randint(0, t - 1)
-        # random.seed(time.time())
         b = random.randint(0, t - 1)
+
         index_list[a], index_list[b] = index_list[b], index_list[a]
 
         return index_list
 
-    def mutation_pop(self, index_list):
-        m_rate = int(0.2 * len(index_list))
-        i = 0
-        while i <= m_rate:
-            print("inside pop m ", i, m_rate)
-            index = random.randint(0, len(index_list) - 1)
-            t = random.randint(0, self.reads_n - 1)
-            print(t)
-            if t not in index_list:
-                index_list[index] = t
-                i += 1
-        print("Mutation POP done !!")
-        return self.mutation(index_list)
+    def crossover(self, a, b):
+        point_of_crossover = random.randint(0, len(a) - 1)
 
-    def find_neighbours(self, index_list_1, index_list_2):
-        neighbours = {}
-        length_1 = len(index_list_1)
-        length_2 = len(index_list_2)
+        index_list = a[:point_of_crossover] + b[point_of_crossover:]
+        genome_new = self.generate_genome(index_list, 0)
 
-        for i, idx in enumerate(index_list_1):
-            if idx not in neighbours:
-                neighbours[idx] = {}
-
-            neighbours[idx].add(index_list_1[i - 1])
-            neighbours[idx].add(index_list_1[(i + 1) % length_1])
-
-        for i, idx in enumerate(index_list_2):
-            if idx not in neighbours:
-                neighbours[idx] = {}
-
-            neighbours[idx].add(index_list_2[i - 1])
-            neighbours[idx].add(index_list_2[(i + 1) % length_2])
-
-        return neighbours
-
-    def crossover_edge_recombination(self, p1, p2):
-        length = len(p1)
-
-        neighbour_list = self.find_neighbours(p1, p2)
-        # print('Neighbour List:', neighbour_list)
-
-        current_node = random.choice((p1[0], p2[0]))
-        # print('Start node:', current_node)
-
-        child = [current_node]
-        while len(child) < length:
-            # print("Inside while", len(child), length)
-            # Remove selected node from neighbour_lists
-            for node in neighbour_list:
-                if current_node in neighbour_list[node]:
-                    neighbour_list[node].remove(current_node)
-
-            min_neigh_list = neighbour_list[current_node]
-            del neighbour_list[current_node]
-
-            if len(min_neigh_list) > 0:  # if the chosen node has any neighbours
-                # get the best match out of neighbours as next
-                max_overlap = self.overlap_matrix[
-                    current_node, max(min_neigh_list, key=lambda x: self.overlap_matrix[current_node, x])]
-                possibilities = list(
-                    filter(lambda x: self.overlap_matrix[current_node, x] == max_overlap, min_neigh_list))
-                current_node = possibilities[random.randint(0, len(possibilities) - 1)]
-            else:
-                # get the best match out of every node as next
-                max_overlap = self.overlap_matrix[
-                    current_node, max(neighbour_list, key=lambda x: self.overlap_matrix[current_node, x])]
-                possibilities = list(
-                    filter(lambda x: self.overlap_matrix[current_node, x] == max_overlap, neighbour_list))
-                current_node = possibilities[random.randint(0, len(possibilities) - 1)]
-            child.append(current_node)  # add the node to the solution
-        return self.generate_genome(child), child
+        return genome_new, index_list
 
 
 if __name__ == '__main__':
     reads = ["ab", "ba", "aaa", "bbb"]
     reads1 = ["abb", "aaa", "ab", "bb"]
+
     u = GeneticAlgorithmUtil(reads, reads1)
-    # print(u.add(0,1,1))
+
     print(u.generate_genome([3,3], 0))
     print(u.generate_genome([3,3], 1))
     print(u.initialize_population(2))
     print(u.fitness_score([1,0,1,1,1]))
-    print(u.crossover1([1,0,1,1,0,0], [1,0,1,1,1,1], 2, 4))
-    print(u.mutation([1,0,1,1,1,1]))
+    print("crossover", u.crossover([1,0,1,1,0,0], [1,0,1,1,1,1]))
+    print("mutation", u.mutation([1,0,1,1,1,1]))
     print("done")
